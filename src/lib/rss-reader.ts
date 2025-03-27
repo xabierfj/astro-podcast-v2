19ff322c56ba74f3119ac129661b71d973754374
Xabier <xabier.figueroa96@gmail.com>
2025-03-27 16:01:29 +0100

    Enhance RSS reader by integrating TitleBeautifier and Slugify for improved episode title formatting

--- src/lib/rss-reader.ts
+++ src/lib/rss-reader.ts
@@ -1,4 +1,4 @@
-import { Truncate, FormattedDate } from "./utils";
+import { FormattedDate, TitleBeautifier,Slugify } from "./utils";
 import Parser from "rss-parser";
 import podcastInfo from './podcast-info';
 
@@ -9,11 +9,11 @@ export const episodes = feed.items.map((item) => {
   const mediaLink = item.enclosure as { url: string, type: string };
   const duration = item.itunes.duration;
   const image = item.itunes.image;
-  const episodeSlug = item.title;
+  const episodeSlug = Slugify(item.title ?? '');
   return {
-    title: item.title,
+    title: TitleBeautifier(item.title ?? ''),
     slug: episodeSlug,
-    description: Truncate(item.contentSnippet, 260),
+    description: item.contentSnippet,
     guid: { text: item.guid },
     publishDate: FormattedDate(item.pubDate as string),
     enclosure: { url: mediaLink.url, type: mediaLink.type },
