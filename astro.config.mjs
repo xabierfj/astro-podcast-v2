bd8697804972c90c11439afa210b5ec3f666cf01
Xabier <xabier.figueroa96@gmail.com>
2025-05-17 15:53:45 +0200

    feat: migrate from Preact to React, updating configurations and dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
-import preact from '@astrojs/preact';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +11,5 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
 
-  integrations: [preact()]
+  integrations: [react()]
 });
